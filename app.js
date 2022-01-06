750f1d6b9dd9bc89be3eaac8bb5909763dc7c958
marievaba <marie.vaba@gmail.com>
2022-01-06 09:29:48 +0200

    Added item to data structure

--- app.js
+++ app.js
@@ -19,6 +19,18 @@ const ItemCtrl = (function(){
         getItems: function(){
             return data.items
         },
+        addItem: function(name, calories){
+            let ID;
+            if (data.items.length > 0){
+                ID = data.items[data.items.length - 1].id + 1
+            } else {
+                ID = 0
+            }
+            calories = parseInt(calories);
+            newItem = new Item(ID, name, calories);
+            data.items.push(newItem);
+            return newItem;
+        },
         logData: function(){
             return data
         }
@@ -26,7 +38,10 @@ const ItemCtrl = (function(){
 })();
 const UICtrl = (function(){
     const UISelectors = {
-        itemList: '#item-list'
+        itemList: '#item-list',
+        itemNameInput:'#item-name',
+        itemCaloriesInput: '#item-calories',
+        addBtn: '.aad-btn'
     }
     return {
         populateItemList: function(items){
@@ -43,11 +58,34 @@ const UICtrl = (function(){
             });
 
             document.querySelector(UISelectors.itemList).innerHTML = html;
+        },
+        getSelectors: function(){
+            return UISelectors;
+        },
+        getItemInput: function(){
+            return{
+                name:document.querySelector(UISelectors.itemNameInput).value, 
+                calories: document.querySelector(UISelectors.itemCaloriesInput).value
+            }
         }
     }
 })();
 
 const App = (function(ItemCtrl, UICtrl){
+    const loadEventListeners = function(){
+        const UISelectors = UICtrl.getSelectors();
+        document.querySelector(UISelectors.addBtn);
+            addEventListener('click', itemAddSubmit);
+    }
+    const itemAddSubmit = function(event){
+        console.log('item add event function');
+        const input = UICtrl.getItemInput();
+        if (input.name !== '' && input.calories !== ''){
+            const newItem = ItemCtrl.addItem(input.name, input.calories);
+            console.log(newItem);
+        }
+        event.preventDefault();
+    }
     return {
         init: function(){
             console.log("Initializing App")
@@ -55,7 +93,8 @@ const App = (function(ItemCtrl, UICtrl){
             const items = ItemCtrl.getItems()
          
             UICtrl.populateItemList(items)
-            console.log(items)
+            
+            loadEventListeners()
         }
     }
 })(ItemCtrl, UICtrl);
